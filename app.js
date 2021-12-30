fccc1e6c1fe28cf36166daeefbf9bc748c36bfd3
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-30 11:44:59 +0100

    step 2

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
-const ArrayCards = ["cards/image (2).png","cards/image (6).png","cards/image (5).png","cards/image (1).png","cards/image (4).png","cards/image (3).png"];
-const ArrayCards2 = ["cards/image (2).png","cards/image (6).png","cards/image (5).png","cards/image (1).png","cards/image (4).png","cards/image (3).png"];
+const ArrayCards = ["cards/image (2)","cards/image (6)","cards/image (5)","cards/image (1)","cards/image (4)","cards/image (3)"];
+const ArrayCards2 = ["cards/image (2)","cards/image (6)","cards/image (5)","cards/image (1)","cards/image (4)","cards/image (3)"];
 
 let choice = 0;
 let index = 0;
 let choiceOne;
 let choiceTwo;
 let timer;
-let numberPairs = 0;
 let pairs = document.getElementById('pairs')
 
 // function to display images
 function displayImg() {
-    for (let i=0; i<=ArrayCards.length-1; i++) {
+    for (let i=0; i<= ArrayCards.length-1 + ArrayCards2.length-1; i++) {
         //Loop that runs through the image array
-        document.getElementById('plate').innerHTML += "couv.png";
-    }
-}
-function displayImg2() {
-    for (let i=0; i<=ArrayCards2.length-1; i++) {
-        //Loop that runs through the image array
-        document.getElementById('plate').innerHTML += "couv.png";
+        document.getElementById('plate').innerHTML += '<img src="couv.png">';
     }
 }
 
 displayImg();
-displayImg2()
 
 function shuffle () {
-    for (let i = 0; i < ArrayCards.length; i++) {
+    for (let i = 0; i < ArrayCards.length-1 + ArrayCards2.length-1; i++) {
 
     }
 }
