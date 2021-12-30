94f270c1931cdf7fb9f9dfb499dba079441cc2b5
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-30 10:57:16 +0100

    step 2

--- app.js
+++ app.js
@@ -1,28 +1,66 @@
-const ArrayCards = ["image (2).png","image (2).png","image (6).png","image (6).png","image (5).png","image (5).png","image (1).png","image (1).png","image (4).png","image (4).png","image (3).png","image (3).png"];
-const ArrayCards2 = ["image (2).png","image (2).png","image (6).png","image (6).png","image (5).png","image (5).png","image (1).png","image (1).png","image (4).png","image (4).png","image (3).png","image (3).png"];
+const ArrayCards = ["cards/image (2).png","cards/image (6).png","cards/image (5).png","cards/image (1).png","cards/image (4).png","cards/image (3).png"];
+const ArrayCards2 = ["cards/image (2).png","cards/image (6).png","cards/image (5).png","cards/image (1).png","cards/image (4).png","cards/image (3).png"];
 
 let choice = 0;
+let index = 0;
 let choiceOne;
 let choiceTwo;
-let card = document.getElementsByClassName('card')
-let img;
+let timer;
+let numberPairs = 0;
+let pairs = document.getElementById('pairs')
 
-//function to display images
+// function to display images
 function displayImg() {
     for (let i=0; i<=ArrayCards.length-1; i++) {
         //Loop that runs through the image array
-        document.getElementById('plate').innerHTML += '<img src="couv.png">'
+        document.getElementById('plate').innerHTML += "couv.png";
     }
 }
+function displayImg2() {
+    for (let i=0; i<=ArrayCards2.length-1; i++) {
+        //Loop that runs through the image array
+        document.getElementById('plate').innerHTML += "couv.png";
+    }
+}
+
 displayImg();
+displayImg2()
+
+function shuffle () {
+    for (let i = 0; i < ArrayCards.length; i++) {
+
+    }
+}
 
 //Function to manage user clicks
-function userChoice (cards) {
+function userChoice (index) {
     if (choice === 2) {
         //Prevent user from clicking more than twice
         return;
     }
+
     if (choice === 0) {
-        choiceOne = cards;
+        choiceOne = index;
+        choice = 1;
+        ArrayCards[index] = index;
+    } else {
+        choice = 2
+        choiceTwo = index;
+        //create a timeout for the player can look the cards
+        timer = setTimeout('check()', 1000);
+    }
+    console.log("aze")
+}
+
+userChoice();
+
+//check if a pair has been found
+function check(index) {
+    choice = 0;
+    if (ArrayCards[index] === ArrayCards2[index]){
+        pairs = +1;
+    }
+    else {
+        pairs = -1;
     }
 }
