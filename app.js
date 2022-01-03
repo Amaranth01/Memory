51e2e40c7f2e7536058c759bb3323980fa6f2b4d
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-03 15:51:16 +0100

    step 5

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let arrayCards = ["cards/image(2)","cards/image(6)","cards/image(5)","cards/image(1)","cards/image(4)","cards/image(3)","cards/image(2)","cards/image(6)","cards/image(5)","cards/image(4)","cards/image(1)","cards/image(3)"];
+let arrayCards = ["cards/image(2).png","cards/image(6).png","cards/image(5).png","cards/image(1).png","cards/image(4).png","cards/image(3).png","cards/image(2).png","cards/image(6).png","cards/image(5).png","cards/image(4).png","cards/image(1).png","cards/image(3).png"];
 let click = 0;
 let index = 0;
 let timer;
+let score = 0;
 let pairs = document.getElementById('pairs');
 let cards = $('.cards');
-let back = $('.back');
 
-
-arrayCards.sort = (()=> 0.5 - Math.random());
+//shuffle the cards
+arrayCards.sort = (() => 0.5 - Math.random());
 console.log(arrayCards); //mélange OK
 
 cards.click(function (){
@@ -15,16 +15,23 @@ cards.click(function (){
 
     //Manage user clicks
     if (click === 1) {
-        const idx = cards.index($(this));
-        this.style.backgroundImage = "url("+ arrayCards[idx] + ".png)";
-    }
-    else if (click === 2 ) {
-        const idx = cards.index($(this));
-        this.style.backgroundImage = "url("+ arrayCards[idx] + ".png)";
-        click = 0;
+        let idx = cards.index($(this));
+        console.log("idx : " + idx);
+        this.style.backgroundImage = `url("` + arrayCards[idx] + `")`;
+        //this.setAttribute("style", `background-image : url("` + arrayCards[idx] + `")`)
+        console.log("aze");
     }
 
-    if (arrayCards[index] === arrayCards[index]) {
-        pairs++;
+    else if (click === 2 ) {
+        let idx = cards.index($(this));
+        console.log("idx : " + idx)
+        this.style.backgroundImage = `url("`+ arrayCards[idx] + `")`;
+        click = 0;
+        console.log("tyu");
+    }
+
+    if (click === click) {
+
+        pairs.innerHTML = score++;
     }
 });
