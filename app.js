423cc31f7ed517c6dbf16b6b0449618194522d4d
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-03 16:47:55 +0100

    step 6

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
 let arrayCards = ["cards/image(2).png","cards/image(6).png","cards/image(5).png","cards/image(1).png","cards/image(4).png","cards/image(3).png","cards/image(2).png","cards/image(6).png","cards/image(5).png","cards/image(4).png","cards/image(1).png","cards/image(3).png"];
-let click = 0;
-let index = 0;
+let arrayStock = [];
 let timer;
 let score = 0;
 let pairs = document.getElementById('pairs');
 let cards = $('.cards');
 
 //shuffle the cards
-arrayCards.sort = (() => 0.5 - Math.random());
+arrayCards.sort = ( () => 0.5 - Math.random());
 console.log(arrayCards); //mélange OK
 
 cards.click(function (){
-    click++;
-
+    let idx = cards.index($(this));
     //Manage user clicks
-    if (click === 1) {
-        let idx = cards.index($(this));
-        console.log("idx : " + idx);
         this.style.backgroundImage = `url("` + arrayCards[idx] + `")`;
-        //this.setAttribute("style", `background-image : url("` + arrayCards[idx] + `")`)
-        console.log("aze");
-    }
 
-    else if (click === 2 ) {
-        let idx = cards.index($(this));
-        console.log("idx : " + idx)
-        this.style.backgroundImage = `url("`+ arrayCards[idx] + `")`;
-        click = 0;
-        console.log("tyu");
-    }
+    arrayStock.push(idx);
 
-    if (click === click) {
-
-        pairs.innerHTML = score++;
+    if (arrayStock.length === 2) {
+        if (arrayCards[arrayStock[0]] === arrayCards[arrayStock[1]]) {
+            score++;
+            pairs.innerHTML = score;
+            arrayStock = [];
+        }
+        else {
+            setTimeout( () =>  {
+                cards[arrayStock[0]].style.backgroundImage = `url("cards/couv.png")`;
+                cards[arrayStock[1]].style.backgroundImage = `url("cards/couv.png")`;
+                arrayStock = [];
+            },700);
+            score--;
+        }
     }
 });
