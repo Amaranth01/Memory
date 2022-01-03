32967ed6f0b93a70ec15f14aaa231c7a77d72a29
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-03 19:05:33 +0100

    Il ne reste que la fenêtre modale et le style

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 let arrayCards = ["cards/image(2).png","cards/image(6).png","cards/image(5).png","cards/image(1).png","cards/image(4).png","cards/image(3).png","cards/image(2).png","cards/image(6).png","cards/image(5).png","cards/image(4).png","cards/image(1).png","cards/image(3).png"];
 let arrayStock = [];
-let timer;
 let score = 0;
 let pairs = document.getElementById('pairs');
 let cards = $('.cards');
 
 //shuffle the cards
 arrayCards.sort = ( () => 0.5 - Math.random());
-console.log(arrayCards); //mélange OK
+console.log(arrayCards); //mélange pas OK
 
 cards.click(function (){
     let idx = cards.index($(this));
@@ -23,12 +22,16 @@ cards.click(function (){
             arrayStock = [];
         }
         else {
+            score--;
+            pairs.innerHTML = score;
             setTimeout( () =>  {
                 cards[arrayStock[0]].style.backgroundImage = `url("cards/couv.png")`;
                 cards[arrayStock[1]].style.backgroundImage = `url("cards/couv.png")`;
                 arrayStock = [];
             },700);
-            score--;
         }
+    }
+    if ( arrayStock[idx] === arrayCards[0]) {
+        alert('vous avez gagné');
     }
 });
